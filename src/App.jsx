b5bc3051cc836b8d40c896cabdc09c5ab4897f36
Vitor Nóbrega <devvitornobrega@gmail.com>
2025-12-11 18:59:11 -0300

    feat: criar componente AddTask inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 export default function App() {
   return (
     <div>
       <h1>Gerenciador de Tarefas</h1>
+      <AddTask />
       <Tasks />
     </div>
   );
